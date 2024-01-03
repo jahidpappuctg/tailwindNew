ec44ec0fd41355c7bf2946a2d5fd1fbc36279f69
jahidpappuctg <jahidpappublog@gmail.com>
2024-01-03 09:58:37 +0600

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,109 @@
+// fetch('oroduct.json')
+// .then(res => res.json())
+// .then()
+
+// function test(){
+//     alert("Salam");
+// }
+
+
+// var btn = document.getElementById("btn");
+
+// btn.addEventListener('click', function(){
+//     alert('hello');
+// });
+// var btn1 = document.getElementById('btn');
+
+// btn1.addEventListener('click', function(){
+
+//     var inputbox = document.getElementById('inputBox');
+//     var h3 = document.getElementById('h3');
+
+//     var inputvalue = inputbox.value;
+
+//     h3.innerText = inputvalue;
+
+// })
+
+// var btn2 = document.getElementById('inputBox');
+
+// inputBox.addEventListener('click', function(){
+
+// })
+
+// var descrip = document.getElementById('descrip');
+
+// descrip.addEventListener();
+
+
+// const info = [
+//     {
+//         id: 1,
+//         productName: 'for xiaomi Redni Note 11',
+//         imageURL : 'https://statc-01.daraz.com/p/s433.jpg',
+//         price: 10000,
+//         despcription: 'lorem ipsoam'
+//     },
+//     {
+//         id: 2,
+//         productName: 'for xiaomi Redni Note 11',
+//         imageURL : 'https://statc-01.daraz.com/p/s433.jpg',
+//         price: 15000,
+//         despcription: 'lorem ipsoam'
+//     },
+//     {
+//         id: 3,
+//         productName: 'for xiaomi Redni Note 11',
+//         imageURL : 'https://statc-01.daraz.com/p/s433.jpg',
+//         price: 14000,
+//         despcription: 'lorem ipsoam'
+//     },
+//     {
+//         id: 1,
+//         productName: 'for xiaomi Redni Note 11',
+//         imageURL : 'https://statc-01.daraz.com/p/s433.jpg',
+//         price: 120000,
+//         despcription: 'lorem ipsoam'
+//     }
+
+// ]
+
+// const form1 = document.getElementById('form');
+
+// form1.addEventListener( 'submit', (e)=>{
+//     e.preventDefault();
+
+
+//     const searchBaxValue = e.target.searchBox.value;
+//         const searchText = searchBaxValue.toLowerCase();
+//         console.log('searchText');
+
+
+//     const searchhedResult = info.filter((value) =>{
+
+//         const productTitle = value.productName;
+//         const title = productTitle.toLowerCase();
+         
+//         return title.includes(searchText);
+
+//         console.log(searchBaxValue);
+
+//     });
+
+// });
+
+
+const arr = [10, 28, 39, 39, 23 ];
+
+const obj = {
+    name : "Raju",
+    mobile : '0167820000',
+    address : "saver, dhaka",
+    role : 'Amdin'
+}
+
+for (let value in arr){
+
+    console.log(arr);
+    
+}
